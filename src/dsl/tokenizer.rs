f41bd1fc29197597e9d19566f653cec1b058bb8c
Timofey Neyenburg <neyenburgz@gmail.com>
2024-02-01 16:16:14 +0200

    wip

--- src/dsl/tokenizer.rs
+++ src/dsl/tokenizer.rs
@@ -1,3 +1,7 @@
+use std::collections::HashMap;
+use std::vec::Vec;
+
+
 pub enum TokenType {
     LITERAL, // 'any word'
     STAR,    // '*'
@@ -6,45 +10,69 @@ pub enum TokenType {
     LBRACK,  // '('
     RBRACK,  // ')'
     SEMICOL, // ';'
+    ILLEGAL  // "ill"
 }
 
 pub struct Token {
     t_type: TokenType,
     value: String,
-    start_pos: i8,
-    end_pos: i8,
+    start_pos: u8,
+    end_pos: u8,
 }
 
 pub struct Lexer {
-    pub pos: i8,
+    pub pos: u8,
     pub current_char: String,
     pub source_string: String,
 }
 
+let TOKEN_TABLE: HashMap<&str, TokenType> = HashMap::from([
+    ("*", TokenType::STAR),
+]);
+
 impl Lexer {
-    pub fn tokenize(&mut self, src: String) {
-        for c in src.chars() {
+    pub fn tokenize(&mut self) {
+        let mut tokens = Vec::new();
+        for c in self.source_string.chars() {
+            let start_pos = self.pos;
             if c.is_alphabetic() {
-                let mut literal = String::new();
-                while c.is_alphabetic() {
-                    literal.push_str(c);
-                    self.pos += 1;
-                }
+                let literal = self.build_literal();
+                tokens.push(Token {
+                    t_type: TokenType::LITERAL,
+                    value: literal,
+                    end_pos: self.pos,
+                    start_pos
+                });
+            } else {
+                tokens.push(Token {
+                    t_type: TokenType::ILLEGAL,
+                    value: "illegal".to_string(),
+                    end_pos: self.pos,
+                    start_pos
+                })
             }
         }
     }
 
-    fn build_literal(self, c: char) -> String {
+    fn build_literal(&mut self) -> String {
         let mut literal = String::new();
+        let mut c = self.next_char();
+
         while c.is_alphabetic() {
-            literal.push_str(&String::from(c));
+            let prep_c = self.char_to_str(c);
+            literal.push_str(prep_c);
             self.pos += 1;
+            c = self.next_char();
         }
         return literal;
     }
 
     fn next_char(self) -> char {
-        return &self.source_string[self.pos];
+        return self.source_string.chars().nth(self.pos.into()).unwrap();
+    }
+
+    fn char_to_str(self, c: char) -> &'static str {
+        return c.to_string().as_str();
     }
 }
 
--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ fn main() {
     let mut lx = dsl::tokenizer::Lexer {
         pos: 0,
         current_char: String::from("byba"),
+        source_string: "select * from table;".to_string()
     };
-    lx.tokenize("select * from table;".to_string());
+    lx.tokenize();
 }
